b2259bbea97a6cb6d97928a7a89fd42a2bb2e9bc
baebae02 <bae4614@gmail.com>
2024-09-25 17:07:34 +0900

    🎉 Init: spring boot setting

---
+++ src/main/kotlin/com/pravo/Pravo/PravoApplication.kt
@@ -0,0 +1,11 @@
+package com.pravo.Pravo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PravoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PravoApplication>(*args)
+}
