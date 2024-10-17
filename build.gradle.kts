bb63927f19c5c279bee058130f54d39eb97379cc
baebae02 <bae4614@gmail.com>
2024-10-17 14:41:35 +0900

    [REFACTOR] Merge deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,19 +37,34 @@ repositories {
 }
 
 dependencies {
+    // Spring Web
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // Health Check
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // Lombok
+    compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    // MySQL
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // H2
+    runtimeOnly("com.h2database:h2")
+
+    // Test & Validation
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // QueryDSL
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
     kapt("jakarta.persistence:jakarta.persistence-api")
@@ -71,4 +86,10 @@ idea {
         sourceDirs.add(kaptMain)
         generatedSourceDirs.add(kaptMain)
     }
+}
+
+ktlint {
+    ignoreFailures.set(false)
+
+    reporters { reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.JSON) }
 }
--- src/main/kotlin/com/pravo/pravo/domain/member/controller/MemberController.java
+++ src/main/kotlin/com/pravo/pravo/domain/member/controller/MemberController.java
@@ -1,16 +1,7 @@
 package com.pravo.pravo.domain.member.controller;
 
-import com.pravo.pravo.domain.member.service.MemberService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
-    private final MemberService memberService;
-    private final PasswordEncoder passwordEncoder;
-
-    public MemberController(MemberService memberService, PasswordEncoder passwordEncoder) {
-        this.memberService = memberService;
-        this.passwordEncoder = passwordEncoder;
-    }
 }
