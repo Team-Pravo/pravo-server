baaf48ab296be1544c98c71aa85ccf78f299a29e
dthcs <dthcsc@gmail.com>
2024-10-21 07:38:14 +0900

    [FEAT] add JWT #11

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,13 +38,17 @@ dependencies {
     // Spring Web
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
     // Health Check
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
