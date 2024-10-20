005ea514c9bb28a2d46b4ce76ef3279cb4135688
dthcs <dthcsc@gmail.com>
2024-10-21 07:13:43 +0900

    [FEAT] add jwt parse filter #11

---
+++ src/main/kotlin/com/pravo/pravo/global/jwt/AuthenticateUser.java
@@ -0,0 +1,11 @@
+package com.pravo.pravo.global.jwt;
+
+public class AuthenticateUser {
+
+    private String memberId;
+
+    public AuthenticateUser(String memberId) {
+        this.memberId = memberId;
+    }
+}
+
