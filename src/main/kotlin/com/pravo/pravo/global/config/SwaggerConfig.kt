dd8262376409f50bc504a445b0b4178c1b681755
baebae02 <bae4614@gmail.com>
2024-10-22 16:15:49 +0900

    [FEAT] Swagger Config

---
+++ src/main/kotlin/com/pravo/pravo/global/config/SwaggerConfig.kt
@@ -0,0 +1,26 @@
+package com.pravo.pravo.global.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+            .servers(listOf(Server().url("/api")))
+            .components(Components())
+            .info(configurationInfo())
+    }
+
+    private fun configurationInfo(): Info {
+        return Info()
+            .title("Pravo API")
+            .description("Pravo API documentation")
+            .version("1.0.0")
+    }
+}
