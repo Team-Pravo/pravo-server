44a634e35ee82340d24ce850c44cf03e03f9300c
baebae02 <bae4614@gmail.com>
2024-10-22 16:16:11 +0900

    [FEAT] Add Swagger Config To PromiseController

--- src/main/kotlin/com/pravo/pravo/domain/promise/controller/PromiseController.kt
+++ src/main/kotlin/com/pravo/pravo/domain/promise/controller/PromiseController.kt
@@ -3,22 +3,25 @@ package com.pravo.pravo.domain.promise.controller
 import com.pravo.pravo.domain.promise.dto.request.PromiseSearchDto
 import com.pravo.pravo.domain.promise.dto.response.PromiseResponseDto
 import com.pravo.pravo.domain.promise.service.PromiseService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import lombok.RequiredArgsConstructor
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ModelAttribute
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/promise")
-@RequiredArgsConstructor
-class PromiseController(private val promiseService: PromiseService) {
+@RequestMapping("/api/promise")
+class PromiseController(
+    private val promiseService: PromiseService
+) : PromiseApi {
+
     @GetMapping
-    fun getPromisesByMember(
-        @ModelAttribute request: PromiseSearchDto,
+    @ResponseStatus(HttpStatus.OK)
+    override fun getPromisesByMember(
         @RequestParam memberId: Long,
+        @RequestParam request: PromiseSearchDto?
     ): List<PromiseResponseDto> {
-        return promiseService.getPromisesByMember(memberId, request.startedAt, request.endedAt)
+        // TODO: 토큰 기반 유저 인증 추가 및 유저 401 에러 처리
+        return promiseService.getPromisesByMember(memberId, request?.startedAt, request?.endedAt)
     }
 }
--- src/main/kotlin/com/pravo/pravo/domain/promise/dto/request/PromiseCreateDto.kt
+++ src/main/kotlin/com/pravo/pravo/domain/promise/dto/request/PromiseCreateDto.kt
@@ -1,9 +1,13 @@
 package com.pravo.pravo.domain.promise.dto.request
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 
 data class PromiseCreateDto(
+    @Schema(description = "약속 이름", example = "약속 이름")
     val name: String,
+    @Schema(description = "약속 일시", example = "2021-08-01T00:00:00")
     val promiseDate: LocalDateTime,
+    @Schema(description = "약속 장소", example = "약속 장소")
     val location: String,
 )
